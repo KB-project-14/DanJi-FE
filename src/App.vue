649a33118bd4cc8fccd2725bef52b0db826a1317
doheun9489 <doheun9489@gmail.com>
2025-07-21 15:33:24 +0900

    feat:페이헤더 UI 구현

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import PayHeader from './components/common/topbar/PayHeader.vue'
+
+const onClose = () => {
+  console.log('닫기 버튼 ')
+}
 </script>
 
 <template>
   <RouterView />
+  <PayHeader @right-click="onClose" />
 </template>
 
 <style scoped></style>
---
+++ src/components/common/topbar/PayHeader.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { XCircle } from 'lucide-vue-next'
+</script>
+
+<template>
+  <div class="d-flex align-items-center bg-white px-3" style="height: 4rem">
+    <!-- 왼쪽 텍스트 -->
+    <div class="flex-grow-1" style="font-size: 1rem">단지Pay</div>
+
+    <!-- 오른쪽 닫기 아이콘 -->
+    <div style="cursor: pointer" @click="$emit('right-click')">
+      <XCircle :size="24" style="color: var(--Gray05)" />
+    </div>
+  </div>
+</template>
