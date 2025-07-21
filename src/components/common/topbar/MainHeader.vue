2fc771092b354b535e156fb7c330ddca10a3312d
doheun9489 <doheun9489@gmail.com>
2025-07-21 17:07:51 +0900

    style: 부트스트랩 수정

--- src/components/common/topbar/MainHeader.vue
+++ src/components/common/topbar/MainHeader.vue
@@ -1,15 +1,34 @@
 <script setup lang="ts">
+// 추후 홈 화면으로 이동 처리 예정
 defineEmits(['left-click'])
 </script>
 
 <template>
-  <div
-    class="d-flex align-items-center justify-content-start bg-white px-3 pt-3 w-100"
-    style="height: 4.5rem"
-  >
+  <div class="main-header">
     <!-- 왼쪽 로고 -->
-    <div style="cursor: pointer" @click="$emit('left-click')" class="ps-4">
-      <img src="@/assets/images/danji-logo-main.png" alt="단지 로고" style="height: 2.5rem" />
+    <div class="main-header-logo" @click="$emit('left-click')">
+      <img src="@/assets/images/danji-logo-main.png" alt="단지 로고" />
     </div>
   </div>
 </template>
+
+<style scoped>
+.main-header {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  background-color: white;
+  padding: 1rem 1rem 0 1rem;
+  width: 100%;
+  height: 4.5rem;
+}
+
+.main-header-logo {
+  padding-left: 1rem;
+  cursor: pointer;
+}
+
+.main-header-logo img {
+  height: 3rem;
+}
+</style>
