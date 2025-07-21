b74d55169c807ade48535580b29f01ac866d18e4
doheun9489 <doheun9489@gmail.com>
2025-07-21 16:16:43 +0900

    feat: 메인 헤더 UI 구현

---
+++ src/components/common/topbar/MainHeader.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineEmits(['left-click'])
+</script>
+
+<template>
+  <div
+    class="d-flex align-items-center justify-content-start bg-white px-3 pt-3 w-100"
+    style="height: 4.5rem"
+  >
+    <!-- 왼쪽 로고 -->
+    <div style="cursor: pointer" @click="$emit('left-click')" class="ps-4">
+      <img src="@/assets/images/Danji-Logo-Main.png" alt="단지 로고" style="height: 2.5rem" />
+    </div>
+  </div>
+</template>
