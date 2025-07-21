bb43bd19e4536cb8164218d7bfde6e9d541537f6
urjimyu <92876819+urjimyu@users.noreply.github.com>
2025-07-21 16:13:35 +0900

    fix: main.css 적용

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
 import './assets/styles/reset.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import './assets/styles/main.css'
