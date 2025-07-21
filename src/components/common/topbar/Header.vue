f55afd3ccef16cb2b021754a01f00bb22b320ae0
doheun9489 <doheun9489@gmail.com>
2025-07-21 14:44:57 +0900

    feat: 헤더 UI 구현(뒤로가기, 닫기버튼)

---
+++ src/components/common/topbar/Header.vue
@@ -0,0 +1,48 @@
+<script lang="ts" setup>
+import { defineProps, defineEmits } from 'vue'
+import { ChevronLeft, XCircle } from 'lucide-vue-next'
+
+const props = defineProps<{
+  title: string
+  showLeftIcon?: boolean
+  showRightIcon?: boolean
+}>()
+
+const emits = defineEmits<{
+  (e: 'left-click'): void
+  (e: 'right-click'): void
+}>()
+
+const onLeftClick = () => emits('left-click')
+const onRightClick = () => emits('right-click')
+</script>
+
+<template>
+  <div
+    class="position-relative d-flex align-items-center justify-content-center border-bottom bg-white px-3"
+    style="height: 4rem"
+  >
+    <!-- 왼쪽 아이콘(뒤로가기) -->
+    <div
+      v-if="showLeftIcon"
+      class="position-absolute start-0 ps-3 d-flex align-items-center"
+      @click="onLeftClick"
+      style="cursor: pointer"
+    >
+      <ChevronLeft :size="24" />
+    </div>
+
+    <!-- 제목 -->
+    <div class="fw-semibold text-center">{{ title }}</div>
+
+    <!-- 오른쪽 아이콘(닫기버튼) -->
+    <div
+      v-if="showRightIcon"
+      class="position-absolute end-0 pe-3 d-flex align-items-center"
+      @click="onRightClick"
+      style="cursor: pointer"
+    >
+      <XCircle :size="1.5 * 16" style="color: var(--Gray05)" />
+    </div>
+  </div>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import './assets/styles/reset.css'
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import './assets/styles/main.css'
 
 import App from './App.vue'
 import router from './router'
