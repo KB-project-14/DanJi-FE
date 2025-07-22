0cf4bdb32d08052af8799952643f0394591a9e08
urjimyu <92876819+urjimyu@users.noreply.github.com>
2025-07-22 12:50:21 +0900

    feat: 레이아웃 ui 구현

---
+++ src/components/layout/Layout.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import type { LayoutProps } from '@/types/types'
+import MainHeader from '../common/topbar/MainHeader.vue'
+import PayHeader from '../common/topbar/PayHeader.vue'
+import Header from '../common/topbar/Header.vue'
+import BottomNav from '../common/bottomnav/BottomNav.vue'
+
+const headerProps = defineProps<LayoutProps>()
+</script>
+<template>
+  <div class="flex flex-col h-screen w-[37.5rem]">
+    <!-- 헤더 타입에 따라 다른 컴포넌트 렌더링 -->
+    <MainHeader v-if="headerProps.headerType === 'main'" />
+    <PayHeader v-else-if="headerProps.headerType === 'pay'" />
+    <Header
+      v-else-if="headerProps.headerType === 'basic'"
+      :title="headerProps.headerTitle"
+      :show-left-icon="headerProps.showLeftIcon"
+      :show-right-icon="headerProps.showRightIcon"
+    />
+    <!-- 헤더 타입이 'none'일 때는 아무것도 렌더링하지 않음 -->
+
+    <BottomNav v-if="headerProps.isBottomNav" class="flex-grow" />
+  </div>
+</template>
+<style scoped></style>
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,2 +1,24 @@
 // 바텀네비 아이템 타입
 export type BottomNaviType = 'wallet' | 'map' | 'qr' | 'badge' | 'my'
+
+// 레이아웃에서 사용하는 헤더 타입
+export type LayoutProps =
+  | {
+      headerType: 'basic'
+      headerTitle: string
+      showLeftIcon?: boolean
+      showRightIcon?: boolean
+      isBottomNav: boolean
+    }
+  | {
+      headerType: 'main'
+      isBottomNav: boolean
+    }
+  | {
+      headerType: 'pay'
+      isBottomNav: boolean
+    }
+  | {
+      headerType: 'none'
+      isBottomNav: boolean
+    }
