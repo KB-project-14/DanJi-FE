a027fa5e770b4620798c98e297608dd17b0ae218
Sohyun <so405@naver.com>
2025-07-22 13:35:21 +0900

    test : msw 브라우저 용 test 코드

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,11 @@ import './assets/styles/main.css'
 import App from './App.vue'
 import router from './router'
 
+if (import.meta.env.DEV) {
+  const { worker } = await import('./mocks/browser.test')
+  await worker.start()
+}
+
 const app = createApp(App)
 
 app.use(createPinia())
