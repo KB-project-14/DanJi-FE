7899ccac5e744e6bfe69ced55fcc28ecb948c91c
doheun9489 <doheun9489@gmail.com>
2025-07-22 11:12:33 +0900

    style: mainheader css 및 높이 개선

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import MainHeader from './components/common/topbar/MainHeader.vue'
 </script>
 
 <template>
   <RouterView />
+  <MainHeader />
 </template>
 
 <style scoped></style>
