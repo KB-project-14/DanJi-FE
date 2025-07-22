072eb7a96dd05b5919f6f7c07912e5e4e03cfe68
doheun9489 <doheun9489@gmail.com>
2025-07-22 14:54:52 +0900

    WIP: 진행 중 작업 저장

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import UserCard from './components/common/card/UserCard.vue'
 </script>
 
 <template>
   <RouterView />
+  <UserCard :balance="45000" backgroundImageUrl="/src/assets/images/gangseo-card.png" />
 </template>
 
 <style scoped></style>
---
+++ src/components/common/card/UserCard.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps<{
+  balance: number
+  backgroundImageUrl: string
+}>()
+</script>
+
+<template>
+  <div
+    class="relative w-full h-48 max-w-sm mx-auto rounded-xl border border-[#d9d9d9] bg-cover bg-center"
+    :style="{ backgroundImage: `url(${backgroundImageUrl})` }"
+  >
+    <!-- 오른쪽 하단 금액 표시 -->
+    <div
+      class="absolute bottom-2 right-3 py-1 px-3 bg-white text-Brown-4 Head02 rounded border border-gray-300"
+    >
+      남은 금액: {{ balance.toLocaleString() }}원
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
