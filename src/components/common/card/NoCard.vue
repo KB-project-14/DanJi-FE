7d381590ecd9deddb2fe8ff2089df6579dfe1548
doheun9489 <doheun9489@gmail.com>
2025-07-22 15:21:48 +0900

    UserCard 작업 중간 저장

---
+++ src/components/common/card/NoCard.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div
+    class="w-full aspect-[1.58/1] max-w-sm mx-auto border border-Gray-2 rounded-xl flex items-center justify-center bg-Gray-0"
+  >
+    <div class="flex flex-col items-center gap-2">
+      <div class="text-3xl text-Gray-4">+</div>
+      <p class="text-sm text-Gray-5">지역화폐 발급하기</p>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- src/components/common/card/UserCard.vue
+++ src/components/common/card/UserCard.vue
@@ -7,14 +7,15 @@ defineProps<{
 
 <template>
   <div
-    class="relative w-full h-48 max-w-sm mx-auto rounded-xl border border-[#d9d9d9] bg-cover bg-center"
-    :style="{ backgroundImage: `url(${backgroundImageUrl})` }"
+    class="relative w-full max-w-sm aspect-[1586/1000] mx-auto rounded-xl bg-cover bg-center"
+    :style="{
+      backgroundImage: `url(${backgroundImageUrl})`,
+      border: '1px solid #d9d9d9',
+    }"
   >
-    <!-- 오른쪽 하단 금액 표시 -->
-    <div
-      class="absolute bottom-2 right-3 py-1 px-3 bg-white text-Brown-4 Head02 rounded border border-gray-300"
-    >
-      남은 금액: {{ balance.toLocaleString() }}원
+    <!-- 오른쪽 하단 잔액 표시 -->
+    <div class="absolute bottom-2 right-3 py-1 px-3 bg-white text-Brown-4 Head02 rounded">
+      남은 금액: {{ balance.toLocaleString() }} 원
     </div>
   </div>
 </template>
