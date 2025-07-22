f066e5b9ca4d458ed2dcc6d01841b142d8db05bc
doheun9489 <doheun9489@gmail.com>
2025-07-22 10:01:32 +0900

    chore/tailwind 설치 및 부트스트랩 삭제

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import './assets/styles/reset.css'
-import 'bootstrap/dist/css/bootstrap.css'
 import './assets/styles/main.css'
 
 import App from './App.vue'
