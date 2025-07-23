3d5bf900c9b6f1b279e8717db5f6368b25d154a0
doheun9489 <doheun9489@gmail.com>
2025-07-23 14:33:32 +0900

    feat: 드래그 라이브러리 설치 및 구현

--- src/components/common/wallet/WalletItem.vue
+++ src/components/common/wallet/WalletItem.vue
@@ -6,24 +6,34 @@ defineProps<{
   balance: number
   bgColorClass?: string
   showMenu?: boolean
+  isDragMode?: boolean
+}>()
+
+const emit = defineEmits<{
+  (e: 'toggle-drag'): void
 }>()
 </script>
+
 <template>
   <div
     class="flex items-center justify-between px-6 py-5 rounded-xl shadow-sm"
     :class="bgColorClass"
   >
-    <!-- 지역화폐카드 이름 -->
-    <div class="Body00 text-Black2">
-      {{ name }}
-    </div>
+    <!-- 이름 -->
+    <div class="Body00 text-Black2">{{ name }}</div>
 
-    <!-- 지역화폐카드 잔액 -->
+    <!-- 잔액 -->
     <div class="Head02 text-Black2">잔액 {{ balance.toLocaleString() }}원</div>
 
-    <!-- 햄버거 (순서변경)-->
+    <!-- 햄버거 (순서변경) -->
+    <!-- 여기 수정 -->
     <div class="text-Black2 w-[20px] flex justify-end">
-      <Menu v-if="showMenu" :size="20" />
+      <Menu
+        v-if="showMenu"
+        :size="20"
+        class="drag-handle cursor-grab"
+        @click.stop="emit('toggle-drag')"
+      />
     </div>
   </div>
 </template>
