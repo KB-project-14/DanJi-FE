1b953647f9bb8b7922b60ecb69c3fc074d4ab55d
doheun9489 <doheun9489@gmail.com>
2025-07-23 13:44:04 +0900

    feat:지역화폐카드 목록 컴포넌트 생성

---
+++ src/components/common/wallet/WalletItem.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { Menu } from 'lucide-vue-next'
+
+defineProps<{
+  name: string
+  balance: number
+  bgColorClass?: string
+}>()
+</script>
+<template>
+  <div
+    class="flex items-center justify-between px-4 py-3 rounded-xl shadow-sm"
+    :class="bgColorClass"
+  >
+    <!-- 지역화폐 이름 -->
+    <div class="font-medium text-sm text-white">
+      {{ name }}
+    </div>
+
+    <!-- 잔액 -->
+    <div class="font-semibold text-sm text-white">잔액 {{ balance.toLocaleString() }}원</div>
+
+    <!-- 햄버거 아이콘 -->
+    <div class="text-white">
+      <Menu :size="20" />
+    </div>
+  </div>
+</template>
