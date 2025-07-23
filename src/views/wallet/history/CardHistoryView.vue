a468c0204920712df7c9e52dff51e71601de035b
doheun9489 <doheun9489@gmail.com>
2025-07-23 17:46:20 +0900

    feat: 이용내역ListItem 컴포넌트 UI 구현

--- src/views/wallet/history/CardHistoryView.vue
+++ src/views/wallet/history/CardHistoryView.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
 import Layout from '@/components/layout/Layout.vue'
+import CardHistoryItem from '@/components/common/history/CardHistoryItem.vue'
 </script>
 <template>
-  <layout :header-type="'basic'" :header-title="'제목'" :is-bottom-nav="false">
+  <layout :header-type="'basic'" :header-title="'제목'" :is-bottom-nav="false" :showLeftIcon="true">
     <template #content>
-      <YourPageContent />
+      <card-history-item
+        comment="가맹점"
+        :amount="10000"
+        direction="EXPENSE"
+        type="PAYMENT"
+        :after-balance="0"
+        created-at="2025-07-18T10:20:40"
+      />
     </template>
   </layout>
 </template>
