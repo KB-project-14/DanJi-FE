4be5610cd5965588be4b5693514265c05893460a
doheun9489 <doheun9489@gmail.com>
2025-07-24 09:31:04 +0900

    feat: List 컴포넌트 구현

--- src/components/common/history/CardHistoryItemList.vue
+++ src/components/common/history/CardHistoryItemList.vue
@@ -1,2 +1,28 @@
-<script setup lang="ts"></script>
-<template></template>
+<script setup lang="ts">
+import CardHistoryItem from './CardHistoryItem.vue'
+
+defineProps<{
+  histories: {
+    comment: string // 가맹점명
+    amount: number // 거래 금액
+    afterBalance: number // 거래 후 잔액
+    direction: 'INCOME' | 'EXPENSE' // 수입, 지출
+    type: 'CHARGE' | 'REFUND' | 'CONVERT' | 'PAYMENT' // 충전, 환불 등등
+    createdAt: string // 결제 시간
+  }[]
+}>()
+</script>
+<template>
+  <div class="flex flex-col">
+    <card-history-item
+      v-for="(history, index) in histories"
+      :key="index"
+      :comment="history.comment"
+      :amount="history.amount"
+      :afterBalance="history.afterBalance"
+      :direction="history.direction"
+      :type="history.type"
+      :createdAt="history.createdAt"
+    />
+  </div>
+</template>
