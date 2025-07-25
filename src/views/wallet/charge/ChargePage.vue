f5d4281573d681e2c1d1d0f3ea4d1776c6984edf
doheun9489 <doheun9489@gmail.com>
2025-07-25 09:35:37 +0900

    feat: 충전하기 페이지 파일 생성

---
+++ src/views/wallet/charge/ChargePage.vue
@@ -0,0 +1,9 @@
+<script lang="ts"></script>
+
+<template>
+  <Layout :header-type="'basic'" :header-title="'제목'" :is-bottom-nav="false">
+    <template #content>
+      <YourPageContent />
+    </template>
+  </Layout>
+</template>
