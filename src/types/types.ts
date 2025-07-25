8050bd0caff42b93e8a1c0e6c421d5d9b446b98b
urjimyu <92876819+urjimyu@users.noreply.github.com>
2025-07-25 11:31:36 +0900

    refactor: 레이아웃 타입 설정 수정

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,24 +1,30 @@
 // 바텀네비 아이템 타입
 export type BottomNaviType = 'wallet' | 'map' | 'qr' | 'badge' | 'my'
 
-// 레이아웃에서 사용하는 헤더 타입
+// 공통 레이아웃에서 사용하는 헤더 타입
+export type HeaderType = 'basic' | 'main' | 'pay' | 'setting'
+
+// 레이아웃 컴포넌트에서 사용하는 공통 타입
+export interface BaseLayoutProps {
+  isBottomNav?: boolean
+}
+
+interface HeaderWithTitle {
+  headerTitle: string
+  showLeftIcon?: boolean
+  showRightIcon?: boolean
+  emitLeftIconClick?: () => void
+  emitRightIconClick?: () => void
+}
+
+interface PayHeaderProps {
+  headerTitle: string
+  showRightIcon?: boolean
+  emitRightIconClick?: () => void
+}
+
+// 헤더 타입에 따른 레이아웃 컴포넌트 props
 export type LayoutProps =
-  | {
-      headerType: 'basic'
-      headerTitle: string
-      showLeftIcon?: boolean
-      showRightIcon?: boolean
-      isBottomNav?: boolean
-    }
-  | {
-      headerType: 'main'
-      isBottomNav?: boolean
-    }
-  | {
-      headerType: 'pay'
-      isBottomNav?: boolean
-    }
-  | {
-      headerType: 'none'
-      isBottomNav?: boolean
-    }
+  | ({ headerType: 'basic' | 'setting' } & HeaderWithTitle & BaseLayoutProps)
+  | ({ headerType: 'main' } & BaseLayoutProps)
+  | ({ headerType: 'pay' } & PayHeaderProps & BaseLayoutProps)
