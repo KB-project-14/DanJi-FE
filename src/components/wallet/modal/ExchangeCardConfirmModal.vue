27546ee0eb31505ff633b7dc1089fb0ced971a8d
doheun9489 <doheun9489@gmail.com>
2025-07-30 09:25:18 +0900

    style: 환전(지역 -> 지역) 모달 버튼 및 여백 수정

--- src/components/wallet/modal/ExchangeCardConfirmModal.vue
+++ src/components/wallet/modal/ExchangeCardConfirmModal.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
 import { computed } from 'vue'
+import { useRouter } from 'vue-router'
+import DanjiButton from '@/components/common/button/DanjiButton.vue'
 import { calculateExchangeRegionToRegion } from '@/utils/exchange'
 
 import { ArrowRight } from 'lucide-vue-next'
+
+const router = useRouter()
 
 const props = defineProps<{
   fromCard: { name: string; percentage: number }
@@ -14,6 +18,12 @@ const emit = defineEmits<{
   (e: 'close'): void
   (e: 'confirm'): void
 }>()
+
+const handleConfirm = () => {
+  console.log('환전 API 연동 예정')
+  emit('confirm')
+  router.push('/home')
+}
 
 // 계산 결과 가져오기
 const exchangeResult = computed(() =>
@@ -32,9 +42,9 @@ const exchangeResult = computed(() =>
 
       <div class="flex items-center justify-between mb-[2rem]">
         <!-- From -->
-        <div class="flex-1 rounded-xl py-[1.6rem] px-[1rem] bg-Gray-1 text-center">
+        <div class="flex-1 rounded-xl py-[1.6rem] px-[1rem] h-[10rem] bg-Gray-1 text-center">
           <p class="pb-[0.6rem] Body02">{{ props.fromCard.name }}</p>
-          <p class="Body03 text-Gray-6 mb-[0.2rem]">
+          <p class="Body03 text-Gray-6 mt-[1rem]">
             환전 예정 금액:
             <span class="text-Yellow-1">{{ props.totalAmount.toLocaleString() }}원</span>
           </p>
@@ -63,17 +73,26 @@ const exchangeResult = computed(() =>
         <span class="text-Black-2">{{ exchangeResult.finalAmount.toLocaleString() }}원</span>
       </div>
       <div class="mb-[1rem] text-center text-Gray-5 Body03">
-        ※ 환전 시 전 카드의 인센티브( {{ props.fromCard.percentage }}% )는 제외되고, 환전할 카드의
-        인센티브( {{ props.toCard.percentage }}% )만 새로 적용됩니다.
+        ※ 환전 시 전 카드의 인센티브( {{ props.fromCard.percentage }}% )는 제외되고, <br />
+        환전할 카드의 인센티브( {{ props.toCard.percentage }}% )만 새로 적용됩니다.
       </div>
 
-      <div class="flex gap-4">
-        <button class="flex-1 py-3 border rounded-lg text-Gray-6" @click="emit('close')">
+      <div class="w-full max-w-[34.3rem] flex gap-3">
+        <danji-button
+          class="px-[0.5rem] !py-[0.4rem] flex-1 h-[5rem] whitespace-nowrap text-center"
+          variant="smallOutline"
+          @click="emit('close')"
+        >
           취소
-        </button>
-        <button class="flex-1 py-3 rounded-lg bg-Brown-4 text-white" @click="emit('confirm')">
-          환전
-        </button>
+        </danji-button>
+
+        <danji-button
+          class="!px-[0.5rem] !py-[0.5rem] flex-1 h-[5rem] whitespace-nowrap text-center"
+          variant="small"
+          @click="handleConfirm"
+        >
+          환전하기
+        </danji-button>
       </div>
     </div>
   </div>
