9dce60c757412bd21db2b58b5c721529c94c5313
urjimyu <92876819+urjimyu@users.noreply.github.com>
2025-08-01 00:30:55 +0900

    feat: vue query 추가 설정

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { useKakao } from 'vue3-kakao-maps/@utils'
 import { VueQueryPlugin } from '@tanstack/vue-query'
+import { queryClient } from '@/api/queryClient'
 
 import './assets/styles/reset.css'
 import './assets/styles/tailwind.css'
@@ -18,7 +19,10 @@ if (import.meta.env.DEV) {
 useKakao(import.meta.env.VITE_KAKAO_APP_API_KEY)
 const app = createApp(App)
 
-app.use(VueQueryPlugin)
+app.use(VueQueryPlugin, {
+  queryClient,
+  enableDevtoolsV6Plugin: true, // Vue DevTools 지원
+})
 app.use(createPinia())
 app.use(router)
 
