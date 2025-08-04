e8b3f4458f9fdb67876615da7297e7a83b204dce
Sohyun <so405@naver.com>
2025-08-04 09:19:36 +0900

    feat : 분리된 composable page에 적용

--- src/views/wallet/create/LocalCardCreateSelectLocalPage.vue
+++ src/views/wallet/create/LocalCardCreateSelectLocalPage.vue
@@ -2,89 +2,76 @@
 import Layout from '@/components/layout/Layout.vue'
 import DanjiSelect from '@/components/common/form/DanjiSelect.vue'
 import DanjiButton from '@/components/common/button/DanjiButton.vue'
-
-import { computed, onMounted, ref } from 'vue'
-import axios from 'axios'
 import router from '@/router'
-import useGetLocals from '@/composables/queries/local/useGetLocals'
+import useLocalSelector from '@/composables/local/useLocalSelector'
 
-// 임시 타입
-interface Region {
-  id: number
-  name: string
+// 지역 선택 로직을 composable로 분리
+const {
+  selectedRegion,
+  selectedCity,
+  regionNames,
+  cityNames,
+  isButtonDisabled,
+  selectedCityId,
+  resetCity,
+} = useLocalSelector()
+
+// 지역 변경 시 시/구/군 초기화
+const handleRegionChange = () => {
+  resetCity()
 }
-
-interface City {
-  id: number
-  name: string
-}
-
-interface LocalData {
-  regions: Region[]
-  cities: Record<string, City[]>
-}
-
-const localData = ref<LocalData>({ regions: [], cities: {} })
-const selectedRegion = ref<string>('')
-const selectedCity = ref<string>('')
-
-const selectedRegionId = computed<number | null>(() => {
-  return localData.value.regions?.find((region) => region.name === selectedRegion.value)?.id ?? null
-})
-
-const regionNames = computed<string[]>(() => {
-  return localData.value.regions?.map((region) => region.name) ?? []
-})
-
-const cityNames = computed<string[]>(() => {
-  const regionId = selectedRegionId.value
-  if (!regionId) return []
-  return localData.value.cities[regionId]?.map((city) => city.name) ?? []
-})
-
-const isButtonDisabled = computed(() => {
-  return !selectedRegion.value || !selectedCity.value
-})
 
 const handleCardCreateConfirm = (): void => {
   router.push({
     name: 'LocalCardCreateDetail',
     params: {
-      region: selectedRegion.value,
-      city: selectedCity.value,
+      id: selectedCityId.value,
     },
   })
 }
-
-const data = useGetLocals()
 </script>
 
 <template>
-  <layout header-type="basic" header-title="지역화폐 카드 발급" showLeftIcon :is-bottom-nav="false">
+  <layout
+    header-type="basic"
+    header-title="지역화폐 카드 발급"
+    :show-left-icon="true"
+    :is-bottom-nav="false"
+  >
     <template #content>
       <div class="mt-[2.5rem] mx-[2.9rem]">
         <div class="flex flex-col">
-          <span class="Head03 text-Black-1">카드를 발급받을 지역을 선택해주세요.{{ data }}</span>
+          <span class="Head03 text-Black-1">카드를 발급받을 지역을 선택해주세요.</span>
           <span class="Body03 text-Gray-5">선택한 지역의 가맹점에서 혜택을 받을 수 있습니다.</span>
         </div>
 
         <!-- 선택 -->
         <div class="mt-[2.5rem]">
           <span class="Body01 text-Brown-3">행정 구역(도) 선택:</span>
-          <danji-select class="mt-[0.4rem]" :options="regionNames" v-model="selectedRegion" />
+          <danji-select
+            class="mt-[0.4rem]"
+            :options="regionNames"
+            v-model="selectedRegion"
+            @update:modelValue="handleRegionChange"
+          />
         </div>
 
         <div class="mt-[3.1rem]">
           <span class="Body01 text-Brown-3">지역 선택:</span>
-          <danji-select class="mt-[0.4rem]" :options="cityNames" v-model="selectedCity" />
+          <danji-select
+            class="mt-[0.4rem]"
+            :options="cityNames"
+            v-model="selectedCity"
+            :disabled="!selectedRegion"
+          />
         </div>
       </div>
 
       <!-- 하단 버튼 -->
       <div class="flex justify-center absolute bottom-[5rem] left-0 right-0">
-        <danji-button variant="large" @click="handleCardCreateConfirm" :disabled="isButtonDisabled"
-          >다음으로</danji-button
-        >
+        <danji-button variant="large" @click="handleCardCreateConfirm" :disabled="isButtonDisabled">
+          다음으로
+        </danji-button>
       </div>
     </template>
   </layout>
