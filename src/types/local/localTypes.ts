ddfa1d6c4f47ef78938b5c76eb58552b6cee7875
Sohyun <so405@naver.com>
2025-08-04 09:16:39 +0900

    feat : 지역화폐 request, response 타입

--- src/types/local/localTypes.ts
+++ src/types/local/localTypes.ts
@@ -5,3 +5,24 @@ export interface localResponseDtoType {
   createdAt: string | null
   updatedAt: string | null
 }
+
+export type BenefitType = 'CASHBACK' | 'DISCOUNT_CHARGE' | 'BONUS_CHARGE'
+
+export interface localcurrencyListRequestDtoType {
+  benefitType: BenefitType | null
+  city: string | null
+  localCurrencyId: string | null
+  province: string | null
+  regionId: string | null
+}
+
+export interface localcurrencyResponseDtoType {
+  createdAt: string
+  updatedAt: string
+  localCurrencyId: string
+  regionId: number
+  name: string
+  benefitType: BenefitType
+  maximum: number
+  percentage: number
+}
