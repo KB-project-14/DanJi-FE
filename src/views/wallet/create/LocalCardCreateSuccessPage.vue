817ac38145502027fbc725394bdab85820a3b38f
Sohyun <so405@naver.com>
2025-08-04 16:38:29 +0900

    style : 카드 발급 성공 페이지 ui 구현

---
+++ src/views/wallet/create/LocalCardCreateSuccessPage.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import Layout from '@/components/layout/Layout.vue'
+import DanjiButton from '@/components/common/button/DanjiButton.vue'
+import CreateSuccess from '@/components/wallet/create/CreateSuccess.vue'
+</script>
+<template>
+  <layout :header-type="'basic'" :header-title="'결제하기'" :is-bottom-nav="false">
+    <template #content>
+      <div class="relative flex flex-col items-center h-full px-[2rem] pt-[6rem] bg-Gray-0">
+        <!-- 발급 성공 -->
+        <create-success />
+
+        <danji-button class="absolute bottom-0 w-[34.3rem] h-[5.8rem] mb-[3rem]"
+          >지갑으로 가기</danji-button
+        >
+      </div>
+    </template>
+  </layout>
+</template>
+<style scoped></style>
