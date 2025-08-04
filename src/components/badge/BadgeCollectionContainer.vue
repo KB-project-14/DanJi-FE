62541f2cd32345abfc252d87bcabdf7ab68bdf82
urjimyu <92876819+urjimyu@users.noreply.github.com>
2025-08-04 14:29:29 +0900

    feat: 스페셜 뱃지 ui 구현

--- src/components/badge/BadgeCollectionContainer.vue
+++ src/components/badge/BadgeCollectionContainer.vue
@@ -2,6 +2,7 @@
 import { ref, computed } from 'vue'
 import type { BadgeCategoryType } from '@/types/badge'
 import LocalBadgeCollection from '@/components/badge/LocalBadgeCollection.vue'
+import SpecialBadgeCollection from '@/components/badge/SpecialBadgeCollection.vue'
 
 let category = ref<BadgeCategoryType>('지역')
 
@@ -22,13 +23,43 @@ const regionBadges = [
 const specialBadges = [
   {
     id: 3,
-    name: '황금 물결',
-    images: ['special1.jpg', 'special2.jpg', 'special3.jpg'],
+    name: '부산',
+    images: 'special1.jpg',
   },
   {
     id: 4,
-    name: '다이아몬드 노래',
-    images: ['special4.jpg', 'special5.jpg', 'special6.jpg'],
+    name: '양산',
+    images: 'special6.jpg',
+  },
+  {
+    id: 5,
+    name: '양양',
+    images: 'special1.jpg',
+  },
+  {
+    id: 6,
+    name: '독도',
+    images: 'special6.jpg',
+  },
+  {
+    id: 5,
+    name: '전주',
+    images: 'special1.jpg',
+  },
+  {
+    id: 6,
+    name: '인천',
+    images: 'special6.jpg',
+  },
+  {
+    id: 5,
+    name: '울릉도',
+    images: 'special1.jpg',
+  },
+  {
+    id: 6,
+    name: '여수',
+    images: 'special6.jpg',
   },
 ]
 
@@ -74,11 +105,15 @@ const changeCategory = (newCategory: BadgeCategoryType) => {
     <div
       class="flex flex-col justify-center items-center w-full h-[32.3rem] pt-[1.4rem] gap-[2.5rem] rounded-b-[1.6rem] bg-White-0"
     >
-      <local-badge-collection
-        :badge-count="badgeCount"
-        :category="category"
-        :badges="currentBadges"
-      />
+      <div class="flex-1 overflow-y-auto flex flex-col items-center justify-center w-full h-full">
+        <!-- 지역 뱃지 내용 섹션 -->
+        <div class="flex justify-between w-[31.7rem] mb-[2rem] Body01">
+          <span>내가 모은 뱃지를 확인해보세요</span><span>총 {{ badgeCount }}개</span>
+        </div>
+        <local-badge-collection v-if="category === '지역'" :badges="regionBadges" />
+        <special-badge-collection v-if="category === '스페셜'" :badges="specialBadges" />
+      </div>
+
       <!-- 뱃지가 없을 때 표시할 내용 -->
       <div
         v-if="currentBadges.length === 0"
