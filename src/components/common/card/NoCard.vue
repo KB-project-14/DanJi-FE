2101294d9153f425fb9db54d0153b03d618901a5
doheun9489 <doheun9489@gmail.com>
2025-08-05 14:08:48 +0900

    feat: 카드 발급 라우터 생성

--- src/components/common/card/NoCard.vue
+++ src/components/common/card/NoCard.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
 import { CirclePlus } from 'lucide-vue-next'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const goCreateCard = () => {
+  router.push('/wallet/card/create')
+}
 </script>
 
 <template>
   <div
     class="w-[275px] shrink-0 aspect-[1.58/1] rounded-xl flex items-center justify-center bg-white border border-solid border-Gray-3"
+    @click="goCreateCard"
   >
     <div class="flex flex-col items-center gap-2">
       <div class="Body02 text-Gray-4"><CirclePlus /></div>
