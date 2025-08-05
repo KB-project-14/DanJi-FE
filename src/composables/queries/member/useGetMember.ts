012feff5ee9cd93074c10d82077bf3ff65fe8325
jihun <wlgns679@gmail.com>
2025-08-05 15:18:23 +0900

    fix: dto 대문자로 변경

--- src/composables/queries/member/useGetMember.ts
+++ src/composables/queries/member/useGetMember.ts
@@ -2,16 +2,16 @@ import { useQuery } from '@tanstack/vue-query'
 import type { ApiResponse } from '@/types/types'
 import { get } from '@/api/api'
 import { MYPAGE_KEYS } from '@/constants/QueryKey'
-import type { memberResponseDtoType } from '@/types/member/memberTypes'
+import type { MemberResponseDto } from '@/types/member/memberTypes'
 import type { AxiosResponse } from 'axios'
 
-export const getMember = async (): Promise<memberResponseDtoType> => {
-  const response: AxiosResponse<ApiResponse<memberResponseDtoType>> = await get('/api/members/me')
+export const getMember = async (): Promise<MemberResponseDto> => {
+  const response: AxiosResponse<ApiResponse<MemberResponseDto>> = await get('/api/members/me')
   return response.data.data
 }
 
 export const useGetMember = () => {
-  const { data } = useQuery<memberResponseDtoType>({
+  const { data } = useQuery<MemberResponseDto>({
     queryKey: [MYPAGE_KEYS.all],
     queryFn: getMember,
     staleTime: 1000 * 60,
--- src/types/member/memberTypes.ts
+++ src/types/member/memberTypes.ts
@@ -1,6 +1,6 @@
 export type Role = 'ROLE_USER' | 'ROLE_ADMIN'
 
-export interface memberResponseDtoType {
+export interface MemberResponseDto {
     memberId: string
     username: string
     name: string
