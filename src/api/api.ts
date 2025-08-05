f84c81dd95405e06316f2edbe75589375b52c364
urjimyu <92876819+urjimyu@users.noreply.github.com>
2025-08-05 16:50:29 +0900

    refactor: 공용함수 수정

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,12 +1,13 @@
 import type { ApiError } from '@/types/types'
 import axios, { type AxiosInstance } from 'axios'
+import type { AxiosResponse } from 'axios'
 
 const BASE_URL = import.meta.env.VITE_APP_BASE_URL
 
 // API Response 공통 타입 - error 등 구체적인 타입은 수정 가능성 있음
 export interface ApiResponse<T> {
-  data?: T
-  error?: ApiError
+  data: T
+  error: ApiError
   status: string
 }
 
@@ -19,22 +20,24 @@ export const instance: AxiosInstance = axios.create({
   },
 })
 
-export function get<T>(...args: Parameters<typeof instance.get>) {
-  return instance.get<T, T>(...args)
+export type ApiResult<T> = AxiosResponse<ApiResponse<T>>
+
+export function get<T>(...args: Parameters<typeof instance.get>): Promise<ApiResult<T>> {
+  return instance.get<ApiResponse<T>>(...args)
 }
 
-export function post<T>(...args: Parameters<typeof instance.post>) {
-  return instance.post<T>(...args)
+export function post<T>(...args: Parameters<typeof instance.post>): Promise<ApiResult<T>> {
+  return instance.post<ApiResponse<T>>(...args)
 }
 
-export function put<T>(...args: Parameters<typeof instance.put>) {
-  return instance.put<T>(...args)
+export function put<T>(...args: Parameters<typeof instance.put>): Promise<ApiResult<T>> {
+  return instance.put<ApiResponse<T>>(...args)
 }
 
-export function patch<T>(...args: Parameters<typeof instance.patch>) {
-  return instance.patch<T, T>(...args)
+export function patch<T>(...args: Parameters<typeof instance.patch>): Promise<ApiResult<T>> {
+  return instance.patch<ApiResponse<T>>(...args)
 }
 
-export function del<T>(...args: Parameters<typeof instance.delete>) {
-  return instance.delete<T>(...args)
+export function del<T>(...args: Parameters<typeof instance.delete>): Promise<ApiResult<T>> {
+  return instance.delete<ApiResponse<T>>(...args)
 }
