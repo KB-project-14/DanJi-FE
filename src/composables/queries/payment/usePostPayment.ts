6b9b028d3d1200d344d5d112bee5d10db199b4c8
urjimyu <aliyah_7@naver.com>
2025-08-06 23:41:09 +0900

    feat: POST 연동

---
+++ src/composables/queries/payment/usePostPayment.ts
@@ -0,0 +1,26 @@
+import { useMutation, useQueryClient } from '@tanstack/vue-query'
+import { PAY_KEYS } from '@/constants/QueryKey'
+import { post } from '@/api/api'
+import { payRequestDtoType, payResponseDtoType } from '@/types/pay/payTypes'
+
+export const postPayment = async (payInfo: payRequestDtoType) => {
+  const response = await post<payResponseDtoType>('/api/payment', payInfo)
+  return response.data.data
+}
+
+const usePostPayment = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: postPayment,
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: [PAY_KEYS] })
+    },
+    onError: (error) => {
+      console.error('결제 요청 실패', error)
+      // 라우팅 연결 예정
+    }
+  })
+}
+
+export default usePostPayment
