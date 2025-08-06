3aa4ef7ade9b390e570857492b6430c19551eeef
Sohyun <so405@naver.com>
2025-08-06 23:15:10 +0900

    feat : 지갑 단건 조회 get api 쿼리 작성

---
+++ src/composables/queries/wallet/useGetWallet.ts
@@ -0,0 +1,36 @@
+import { useQuery } from '@tanstack/vue-query'
+import { get } from '@/api/api'
+import { WALLET_KEYS } from '@/constants/QueryKey'
+import type { WalletResponseDtoType } from '@/types/wallet/WalletResponseDtoType'
+import { computed } from 'vue'
+
+export const getWallet = async (walletId: string): Promise<WalletResponseDtoType> => {
+  const response = await get<WalletResponseDtoType>(`/api/wallets/${walletId}`)
+  return response.data.data
+}
+
+const useGetWallet = (walletId: string) => {
+  const { data } = useQuery<WalletResponseDtoType>({
+    queryKey: WALLET_KEYS.transactions(walletId),
+    queryFn: () => getWallet(walletId),
+    staleTime: 1000 * 60,
+  })
+
+  return computed<WalletResponseDtoType>(
+    () =>
+      data.value ?? {
+        walletId: '',
+        walletType: 'LOCAL',
+        localCurrencyId: '',
+        localCurrencyName: '',
+        benefitType: 'BONUS_CHARGE',
+        percentage: 0,
+        balance: 0,
+        displayOrder: 0,
+        backgroundImageUrl: '',
+        maximum: 0,
+      },
+  )
+}
+
+export default useGetWallet
