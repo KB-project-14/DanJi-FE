cc8143b0fe34acf31c87dd567a0c3484232bd5da
doheun9489 <doheun9489@gmail.com>
2025-08-07 14:00:42 +0900

    fix: 코드 리뷰 반영

--- src/composables/queries/wallet/patchWalletOrder.ts
+++ src/composables/queries/wallet/patchWalletOrder.ts
@@ -1,10 +1,8 @@
 import { patch } from '@/api/api'
-import type { WalletOrderItem, ApiResponse, UpdatedWallet } from '@/types/wallet/WalletOrder'
+import type { WalletOrderItem, UpdatedWallet } from '@/types/wallet/WalletOrder'
 
 export const updateWalletOrder = async (walletOrderList: WalletOrderItem[]) => {
-  const response = await patch<ApiResponse<UpdatedWallet[]>>('/api/wallets/order', walletOrderList)
-
-  return response
+  return await patch<UpdatedWallet[]>('/api/wallets/order', walletOrderList)
 }
 
 export default updateWalletOrder
--- src/types/wallet/WalletOrder.ts
+++ src/types/wallet/WalletOrder.ts
@@ -4,23 +4,6 @@ export interface WalletOrderItem {
   walletId: string
 }
 
-// 백엔드 응답 공통 타입
-export interface ApiError {
-  code: string
-  errors: Array<{
-    field: string
-    reason: string
-  }>
-  message: string
-  method: string
-}
-
-export interface ApiResponse<T> {
-  data: T
-  error?: ApiError
-  status: string
-}
-
 // 지갑 응답 데이터 타입
 export interface UpdatedWallet {
   balance: number
