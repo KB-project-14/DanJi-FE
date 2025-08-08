ca12772d513c6e05b58ae203af4d1b4b8dca7c70
Sohyun <so405@naver.com>
2025-08-08 16:49:05 +0900

    fix : 레아이웃 크기 변경

--- tailwind.config.js
+++ tailwind.config.js
@@ -69,6 +69,10 @@ export default {
         'fade-in-scale': 'fade-in-scale 0.25s ease-out forwards',
         'fade-out-scale': 'fade-out-scale 0.25s ease-in forwards',
       },
+      height: {
+        'screen-mobile': ['100vh', '100dvh'],
+        'screen-dynamic': 'calc(var(--vh, 1vh) * 100)',
+      },
     },
   },
   plugins: [
