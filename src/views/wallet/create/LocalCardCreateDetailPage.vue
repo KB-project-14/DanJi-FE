c581c7e504a84acf7549b835ddf0aba91758f14d
Sohyun <so405@naver.com>
2025-08-08 11:23:30 +0900

    style : 카드발급 페이지 배경 흰색 처리

--- src/views/wallet/create/LocalCardCreateDetailPage.vue
+++ src/views/wallet/create/LocalCardCreateDetailPage.vue
@@ -74,44 +74,46 @@ const handleCompeleteClick = () => {
 <template>
   <layout header-type="basic" header-title="지역화폐 카드 발급" showLeftIcon :is-bottom-nav="false">
     <template #content>
-      <!-- 상단 구분선 -->
-      <div class="w-full h-[0.1rem] bg-Gray-0" />
+      <div class="bg-White-1">
+        <!-- 상단 구분선 -->
+        <div class="w-full h-[0.1rem] bg-Gray-0" />
 
-      <!-- 지역 선택 칩 -->
-      <danji-chip
-        class="ms-[2.7rem] mt-[1.2rem]"
-        :is-active="true"
-        :is-icon="true"
-        @click="handleClickModal"
-      >
-        {{ localInfo.value?.city ?? localInfo.value?.province }}
-      </danji-chip>
-
-      <!-- 지역 필터 모달 -->
-      <local-filter-modal
-        :is-visible="isModalVisible"
-        v-bind:initial-region="selectedRegion"
-        v-bind:initial-city="selectedCity"
-        @confirm="handleModalConfirm"
-      />
-
-      <!-- 카드 이미지 & 이름 컴포넌트 -->
-      <card-info
-        :card-name="localCurrencyName"
-        :card-image="`http://danji.cloud${benefitInfo?.img}`"
-      />
-
-      <!-- 카드 혜택 정보 컴포넌트 -->
-      <card-benefit-info
-        :incentive-text="benefitDescription"
-        :max-charge-amount="String(benefitInfo?.maximum ?? 0)"
-      />
-
-      <!-- 하단 버튼 -->
-      <div class="flex justify-center mt-[3.4rem] mb-[4rem]">
-        <danji-button variant="large" @click="handleCompeleteClick" :disabled="isPending"
-          >발급하기</danji-button
+        <!-- 지역 선택 칩 -->
+        <danji-chip
+          class="ms-[2.7rem] mt-[1.2rem]"
+          :is-active="true"
+          :is-icon="true"
+          @click="handleClickModal"
         >
+          {{ localInfo.value?.city ?? localInfo.value?.province }}
+        </danji-chip>
+
+        <!-- 지역 필터 모달 -->
+        <local-filter-modal
+          :is-visible="isModalVisible"
+          v-bind:initial-region="selectedRegion"
+          v-bind:initial-city="selectedCity"
+          @confirm="handleModalConfirm"
+        />
+
+        <!-- 카드 이미지 & 이름 컴포넌트 -->
+        <card-info
+          :card-name="localCurrencyName"
+          :card-image="`http://danji.cloud${benefitInfo?.img}`"
+        />
+
+        <!-- 카드 혜택 정보 컴포넌트 -->
+        <card-benefit-info
+          :incentive-text="benefitDescription"
+          :max-charge-amount="String(benefitInfo?.maximum ?? 0)"
+        />
+
+        <!-- 하단 버튼 -->
+        <div class="flex justify-center mt-[3.4rem] mb-[4rem]">
+          <danji-button variant="large" @click="handleCompeleteClick" :disabled="isPending"
+            >발급하기</danji-button
+          >
+        </div>
       </div>
     </template>
   </layout>
